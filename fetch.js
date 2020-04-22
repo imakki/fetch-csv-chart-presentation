04c49fa36179a57adf7d822c9009a80c099706e3
Ankit <ankityc143>
2020-04-22 12:52:22 +0530

    charted the data from csv

--- fetch.js
+++ fetch.js
@@ -1,18 +1,51 @@
-getData();
+const xlabels = [];
+const ytemps = [];
 
-async function getData() {
-    const response = await fetch('ZonAnn.Ts+dSST.csv');
-    const data = await response.text();
-    
-    const table = data.split('\n').slice(1);
+     chartIt();
 
-    table.forEach(row => {
-        const columns = row.split(',');
-        const year = columns[0];
-        const temp = columns[1];
-        console.log(year,temp);
-    });
+        async function chartIt(){
+        await getData();
+        const ctx = document.getElementById('chart').getContext('2d');
+        const myChart = new Chart(ctx, {
+            type: 'line',
+            data: {
+                labels: xlabels,
+                datasets: [{
+                    label: 'Combined Land-Surface Air and Sea-Surface Water Temperature in C°',
+                    data: ytemps,
+                    fill: false,
+                    backgroundColor: 'rgba(255, 99, 132, 0.2)',
+                    borderColor: 'rgba(255, 99, 132, 1)',
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            callback: function(value, index, values) {
+                                return value + '°';
+                            }
+                        }
+                    }]
+                }
+            },
+        });
+        }
 
-    console.log(table);
-    
-}
+        async function getData() {
+            const response = await fetch('ZonAnn.Ts+dSST.csv');
+            const data = await response.text();
+            
+            const table = data.split('\n').slice(1);
+
+            table.forEach(row => {
+                const columns = row.split(',');
+                const year = columns[0];
+                xlabels.push(year);
+                const temp = columns[1];
+                ytemps.push(parseFloat(temp) + 14);
+                console.log(year,temp);
+            });
+            
+        }
